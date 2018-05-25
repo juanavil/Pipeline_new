add5714222c9c9a38b8cbf10a8c197ceb256eb3c
juanavil <juanavil@mx1.ibm.com>
2018-05-25 12:05:48 -0500

    requesting server information

--- pipeline_hellow.sh
+++ pipeline_hellow.sh
@@ -3,3 +3,6 @@
 echo "hello world"
 echo "this is another testing on adding the line"
 ls -ltra
+#############################Server running on information################################
+cat /etc/*release
+uname -r
